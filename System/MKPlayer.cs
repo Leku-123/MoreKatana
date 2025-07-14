958d7c289de2215a123f8546cbf2160d83f788b1
かつ丼さん <109509123+Katudonsan@users.noreply.github.com>
2025-07-14 14:03:53 +0900

    Metal Katana Reworked.

--- System/MKPlayer.cs
+++ System/MKPlayer.cs
@@ -1,13 +1,61 @@
 ﻿using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace MoreKatana.System
 {
     public class MKPlayer : ModPlayer
     {
+        // バニラ刀 //
+        /// <summary>
+        /// カタナ
+        /// </summary>
         public bool EquipKatana = false;
+        /// <summary>
+        /// ムラマサ
+        /// </summary>
         public bool EquipMuramasa = false;
+
+
+        // モアカタナ //
+
+        // 木材刀
+        /// <summary>
+        /// 木材の刀
+        /// </summary>
         public bool EquipWoodenKatana = false;
+
+        // プレハード金属刀
+
+        /// <summary>
+        /// 銅の刀
+        /// </summary>
+        public bool EquipCopperKatana = false;
+        /// <summary>
+        /// 金の刀
+        /// </summary>
+        public bool EquipGoldKatana = false;
+        /// <summary>
+        /// 鉛の刀
+        /// </summary>
+        public bool EquipLeadKatana = false;
+        /// <summary>
+        /// プラチナの刀
+        /// </summary>
+        public bool EquipPlatinumKatana = false;
+        /// <summary>
+        /// 銀の刀
+        /// </summary>
+        public bool EquipSilverKatana = false;
+        /// <summary>
+        /// 錫の刀
+        /// </summary>
+        public bool EquipTinKatana = false;
+        /// <summary>
+        /// タングステンの刀
+        /// </summary>
+        public bool EquipTungstenKatana = false;
+
 
         public override void ResetEffects()
         {
@@ -19,29 +67,67 @@ namespace MoreKatana.System
         public override void PreUpdate()
         {
             if (EquipKatana)
-                KatanaUpdate();
+            {
+                Player.statDefense += 2;
+                Player.endurance += 0.05f;
+            }
+
+            if (EquipCopperKatana)
+                Player.statDefense += 1;
+
+            if (EquipTinKatana)
+                Player.statDefense += 2;
+
+            if (EquipTungstenKatana)
+            {
+                Player.statDefense += 5;
+                Player.fireWalk = true;
+                Player.buffImmune[BuffID.OnFire] = true;
+            }
+
+            if (EquipPlatinumKatana)
+                Player.statDefense += 7;
+
+            if (EquipLeadKatana)
+            {
+                Player.statDefense += 4;
+                Player.moveSpeed *= 0.9f;
+            }
+
+            if (EquipSilverKatana)
+                Player.statDefense += 2;
+
+            if (EquipGoldKatana)
+            {
+                long coin = Utils.CoinsCount(out bool over, Player.inventory);
+                int bonus = 0;
+                if (over || coin >= 10000)
+                    bonus = 10;
+                else if (coin > 999)
+                {
+                    bonus = (int)(coin /= 1000);
+                }
+                Player.statDefense += (3 + bonus);
+            }
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if (EquipCopperKatana)
+                CopperKatana(target);
         }
 
         public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
         {
-            if (EquipWoodenKatana)
-                WoodenKatana(in target, ref modifiers);
             if (EquipKatana)
-                Katana(in target, ref modifiers);
+                Katana(target, ref modifiers);
 
-        }
+            if (EquipWoodenKatana)
+                WoodenKatana(target, ref modifiers);
 
-        public void WoodenKatana(in NPC target, ref NPC.HitModifiers modifiers)
-        {
-            if (target.life < 1)
-                return;
-            modifiers.SetMaxDamage(target.life - 1);
-        }
+            if (EquipTinKatana && !target.friendly)
+                target.damage -= 1;
 
-        public void KatanaUpdate()
-        {
-            Player.statDefense += 2;
-            Player.endurance += 0.05f;
         }
 
         public void Katana(in NPC target, ref NPC.HitModifiers modifiers)
@@ -59,6 +145,18 @@ namespace MoreKatana.System
             target.SimpleStrikeNPC(dam / 10, modifiers.HitDirection);
 
             Player.GetArmorPenetration(heldItem.DamageType) = armorPen;
+        }
+
+        public void WoodenKatana(in NPC target, ref NPC.HitModifiers modifiers)
+        {
+            if (target.life < 1)
+                return;
+            modifiers.SetMaxDamage(target.life - 1);
+        }
+
+        public void CopperKatana(NPC target)
+        {
+            target.AddBuff(BuffID.Poisoned, 60 * Main.rand.Next(5, 11));
         }
     }
 }
