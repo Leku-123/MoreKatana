39b367c83995ed91c8b756933f8c7e83998610f0
Moririn <164594568+Moririn115@users.noreply.github.com>
2025-07-15 02:39:57 +0900

    add CD Debuff

---
+++ Buffs/KatanaArtsCD.cs
@@ -0,0 +1,16 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace KatanaTest.Buffs
+{
+    public class KatanaArtsCD : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.debuff[Type] = true;
+            Main.buffNoSave[Type] = true;
+            BuffID.Sets.NurseCannotRemoveDebuff[Type] = true;
+        }
+    }
+}
