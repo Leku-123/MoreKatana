9367cf7108fd9b7dce75bc49cd418c6b74cb3896
かつ丼さん <109509123+Katudonsan@users.noreply.github.com>
2025-07-14 15:35:14 +0900

    Silver Katana Update.

--- Items/Katana/Metal/SilverKatana.cs
+++ Items/Katana/Metal/SilverKatana.cs
@@ -1,10 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using MoreKatana.System;
 using Terraria;
-using Terraria.DataStructures;
 using Terraria.ID;
-using Terraria.IO;
-using Terraria.ModLoader;
-using Terraria.UI;
 
 namespace MoreKatana.Items.Katana.Metal
 {
@@ -31,6 +27,28 @@ namespace MoreKatana.Items.Katana.Metal
                 .AddIngredient(ItemID.SilverBar, 10)//銀インゴット１０個を
                 .AddTile(TileID.Anvils)//金床で使うことで
                 .Register();//製作可能にする
+        }
+
+        public override void ModifyHitNPC(Player player, NPC target, ref NPC.HitModifiers modifiers)
+        {
+            if (
+                  target.type == NPCID.Vampire
+               || target.type == NPCID.VampireBat
+               || target.type == NPCID.Werewolf
+               )
+            {
+                modifiers.SetInstantKill();
+            }
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<MKPlayer>().EquipSilverKatana = true;
+        }
+
+        public override void HoldItem(Player player)
+        {
+            player.GetModPlayer<MKPlayer>().EquipSilverKatana = true;
         }
     }
 }
--- System/MKPlayer.cs
+++ System/MKPlayer.cs
@@ -130,18 +130,6 @@ namespace MoreKatana.System
 
             if (EquipWoodenKatana)
                 WoodenKatana(target, ref modifiers);
-
-            if (EquipSilverKatana)
-            {
-                if (
-                       target.type == NPCID.Vampire
-                    || target.type == NPCID.VampireBat
-                    || target.type == NPCID.Werewolf
-                   )
-                {
-                    modifiers.SetInstantKill();
-                }
-            }
         }
 
         public void Katana(in NPC target, ref NPC.HitModifiers modifiers)
