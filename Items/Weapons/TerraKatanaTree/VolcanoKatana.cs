cbace4b36e0abe328cb8b595809779fecf0c6775
かつ丼さん <109509123+Katudonsan@users.noreply.github.com>
2025-08-04 08:24:06 +0900

    ボルケーノ刀

--- Items/Weapons/TerraKatanaTree/VolcanoKatana.cs
+++ Items/Weapons/TerraKatanaTree/VolcanoKatana.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace MoreKatana.Items.Weapons.TerraKatanaTree
 {
@@ -10,6 +12,10 @@ namespace MoreKatana.Items.Weapons.TerraKatanaTree
 
         public override void SetDefaultsItem()
         {
+            Item.DamageType = DamageClass.Melee;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.autoReuse = true;
+            Item.useTurn = true;
             Item.width = 54;
             Item.height = 64;
             Item.useTime = 40;
@@ -21,7 +27,16 @@ namespace MoreKatana.Items.Weapons.TerraKatanaTree
             Item.rare = ItemRarityID.Orange;
 
             //WIP 仮で発射
-            Item.MKItem().SetKatanaDefaults(Item, 0, type: ProjectileID.BallofFire);
+            Item.shoot = ProjectileID.BallofFire;
+            Item.shootSpeed = 10f;
+        }
+
+        public override void PassiveSkill(Player player, bool equipment)
+        {
+            player.buffImmune[BuffID.Chilled] = true;
+            player.buffImmune[BuffID.Frostburn] = true;
+            player.buffImmune[BuffID.Frostburn2] = true;
+            player.buffImmune[BuffID.Frozen] = true;
         }
 
         public override void MeleeEffects(Player player, Rectangle hitbox)
@@ -39,6 +54,21 @@ namespace MoreKatana.Items.Weapons.TerraKatanaTree
         {
             if (Main.rand.NextBool(2))
                 target.AddBuff(BuffID.OnFire, 60 * 3);
+
+            if (hit.Damage >= 1)
+                if (player.ZoneSnow)
+                    hit.Damage *= 2;
+
+            if (player.ItemAnimationJustStarted && Main.myPlayer == Item.whoAmI && (target == null || target.HittableForOnHitRewards()))
+            {
+                Vector2 center = target.Center;
+                Projectile.NewProjectile(player.GetSource_ItemUse(Item), center.X, center.Y, 0f, -1f * player.gravDir, ProjectileID.Volcano, Item.damage, Item.knockBack, player.whoAmI, 0f, 2f, 0f);
+            }
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            return base.Shoot(player, source, position, velocity, type, damage, knockback);
         }
     }
 }
