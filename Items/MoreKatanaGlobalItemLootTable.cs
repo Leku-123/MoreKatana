67985871dbf8dc3195dbf706d2c53f3484778f70
かつ丼さん <109509123+Katudonsan@users.noreply.github.com>
2025-08-17 16:17:33 +0900

    エンチャント刀がクレートからも出るように

---
+++ Items/MoreKatanaGlobalItemLootTable.cs
@@ -0,0 +1,25 @@
+﻿using MoreKatana.Items.Weapons.Misc;
+using Terraria;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreKatana.Items
+{
+    public class MoreKatanaGlobalItemLootTable : GlobalItem
+    {
+        public override void ModifyItemLoot(Item item, ItemLoot itemLoot)
+        {
+            if (item.type == ItemID.GoldenCrate)//金のクレート
+            {
+                //1/30でエンチャント刀が1本出てくる（幸運の影響を受けない）
+                itemLoot.Add(ItemDropRule.OneFromOptionsNotScalingWithLuck(30, ModContent.ItemType<EnchantedKatana>()));
+            }
+            if (item.type == ItemID.GoldenCrateHard)//チタニウムのクレート
+            {
+                //1/15でエンチャント刀が1本出てくる（幸運の影響を受けない）
+                itemLoot.Add(ItemDropRule.OneFromOptionsNotScalingWithLuck(15, ModContent.ItemType<EnchantedKatana>()));
+            }
+        }
+    }
+}
--- Projectiles/Misc/EnchantedKatanaBeam.cs
+++ Projectiles/Misc/EnchantedKatanaBeam.cs
@@ -10,38 +10,6 @@ using Terraria.ModLoader;
 
 namespace MoreKatana.Projectiles.Misc
 {
-    /*public class EnchantedKatanaBeam : ModProjectile
-    {
-        public int CollidingLeft = 3;
-
-        public override void SetDefaults()
-        {
-            Projectile.CloneDefaults(ProjectileID.EnchantedBeam);
-            Projectile.aiStyle = ProjAIStyleID.Beam;
-            AIType = ProjectileID.EnchantedBeam;
-
-            Projectile.DamageType = DamageClass.Melee;
-        }
-
-        public override void OnSpawn(IEntitySource source)
-        {
-            if (Projectile.ai[2] != 0f)
-                Projectile.tileCollide = false;
-        }
-
-        public override bool OnTileCollide(Vector2 oldVelocity)
-        {
-            if (CollidingLeft <= 0)
-                return true;
-
-            Projectile.direction *= -1;
-            Projectile.velocity = Projectile.velocity.RotatedByRandom(MathHelper.ToRadians(90f));
-
-            CollidingLeft--;
-            return false;
-        }
-    }*/
-
     public class EnchantedKatanaBeam : ModProjectile
     {
         private int AttackType
